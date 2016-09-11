5e18e1eb5f612dc52796337d6bc41547ed3f4239
lsachaabazan@gmail.com <ak3rediF>
2016-09-12 00:26:11 +0200

    Cambio spinner por expandablelistview para mejor interfaz

--- app/src/main/java/sachonidas/myapplication/MainActivityLibro.java
+++ app/src/main/java/sachonidas/myapplication/MainActivityLibro.java
@@ -1,7 +1,9 @@
 package sachonidas.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivityLibro extends AppCompatActivity {
@@ -13,9 +15,10 @@ public class MainActivityLibro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_libro);
 
-        imgManos = (ImageView)findViewById(R.id.imgManos);
 
-        imgManos.setImageResource(R.drawable.slide);
-
+    }
+    public void lanzaInicio(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
