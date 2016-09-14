434f871ea39ad79d91bf4f5f46e54a631047f535
lsachaabazan@gmail.com <ak3rediF>
2016-09-14 23:17:16 +0200

    Cambio spinner por expandablelistview para mejor interfaz

--- app/src/main/java/sachonidas/myapplication/MainActivityGaleria.java
+++ app/src/main/java/sachonidas/myapplication/MainActivityGaleria.java
@@ -1,58 +1,98 @@
 package sachonidas.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivityGaleria extends AppCompatActivity {
 
 
-    private ImageView imgV,imgV2,imgV3,imgV4,imgV5,imgV6,imgV7,imgV8,imgV9,imgV10,imgV11,imgV12,imgV13,imgV14,imgV15;
     private ImageButton btnVolver;
+    Integer[] imageIDs = {
+            R.drawable.galeria1,
+            R.drawable.galeria2,
+            R.drawable.galeria3,
+            R.drawable.galeria4,
+            R.drawable.galeria5,
+            R.drawable.galeria6,
+            R.drawable.galeria7,
+            R.drawable.galeria8,
+            R.drawable.galeria9,
+            R.drawable.galeria10,
+            R.drawable.galeria11,
+            R.drawable.galeria12,
+            R.drawable.galeria13,
+            R.drawable.galeria14,
+            R.drawable.galeria15
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_galeria);
 
-        imgV = (ImageView)findViewById(R.id.imageView);
-        imgV2 = (ImageView)findViewById(R.id.imageView2);
-        imgV3 = (ImageView)findViewById(R.id.imageView3);
-        imgV4 = (ImageView)findViewById(R.id.imageView4);
-        imgV5 = (ImageView)findViewById(R.id.imageView5);
-        imgV6 = (ImageView)findViewById(R.id.imageView6);
-        imgV7 = (ImageView)findViewById(R.id.imageView7);
-        imgV8 = (ImageView)findViewById(R.id.imageView8);
-        imgV9 = (ImageView)findViewById(R.id.imageView9);
-        imgV10 = (ImageView)findViewById(R.id.imageView10);
-        imgV11 = (ImageView)findViewById(R.id.imageView11);
-        imgV12 = (ImageView)findViewById(R.id.imageView12);
-        imgV13 = (ImageView)findViewById(R.id.imageView13);
-        imgV14 = (ImageView)findViewById(R.id.imageView14);
-        imgV15 = (ImageView)findViewById(R.id.imageView15);
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        gallery.setAdapter(new ImageAdapter(this));
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                Toast.makeText(getBaseContext(),"pic" + (position + 1) + " selected",
+                        Toast.LENGTH_SHORT).show();
+                // display the images selected
+                ImageView imageView = (ImageView) findViewById(R.id.image1);
+                imageView.setImageResource(imageIDs[position]);
+            }
+        });
 
-        imgV.setImageResource(R.drawable.galeria1);
-        imgV2.setImageResource(R.drawable.galeria2);
-        imgV3.setImageResource(R.drawable.galeria3);
-        imgV4.setImageResource(R.drawable.galeria4);
-        imgV5.setImageResource(R.drawable.galeria5);
-        imgV6.setImageResource(R.drawable.galeria6);
-        imgV7.setImageResource(R.drawable.galeria7);
-        /*imgV8.setImageResource(R.drawable.galeria8);
-        imgV9.setImageResource(R.drawable.galeria9);
-        imgV10.setImageResource(R.drawable.galeria10);
-        imgV11.setImageResource(R.drawable.galeria11);
-        imgV12.setImageResource(R.drawable.galeria12);
-        imgV13.setImageResource(R.drawable.galeria13);
-        imgV14.setImageResource(R.drawable.galeria14);
-        imgV15.setImageResource(R.drawable.galeria15);*/
+
 
         btnVolver = (ImageButton)findViewById(R.id.imageButton);
 
         btnVolver.setImageResource(R.drawable.homeicon);
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+        private int itemBackground;
+        public ImageAdapter(Context c)
+        {
+            context = c;
+            // sets a grey background; wraps around the images
+            TypedArray a = obtainStyledAttributes(R.styleable.MyGallery);
+            itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
+            a.recycle();
+        }
+        // returns the number of images
+        public int getCount() {
+            return imageIDs.length;
+        }
+        // returns the ID of an item
+        public Object getItem(int position) {
+            return position;
+        }
+        // returns the ID of an item
+        public long getItemId(int position) {
+            return position;
+        }
+        // returns an ImageView view
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(imageIDs[position]);
+            imageView.setLayoutParams(new Gallery.LayoutParams(100, 100));
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
     }
 
     public void lanzaInicio(View view){
