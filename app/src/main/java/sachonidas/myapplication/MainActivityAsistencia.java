b03e93af7c1d2aae53601775d54897c425cea835
lsachaabazan@gmail.com <ak3rediF>
2016-09-16 01:39:42 +0200

    Cambio spinner por expandablelistview para mejor interfaz

--- app/src/main/java/sachonidas/myapplication/MainActivityAsistencia.java
+++ app/src/main/java/sachonidas/myapplication/MainActivityAsistencia.java
@@ -3,22 +3,50 @@ package sachonidas.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivityAsistencia extends AppCompatActivity {
 
     private EditText etNombres, etMail, etComentarios;
-    private ToggleButton tglBtnAceptar,tglBtnRechazar,tglBtn1,tglBtn2, tglBtn3,tglBtn4,tglBtn5;
     private Button btnEnviar;
+    String[] TO = {"lsachaabazan@gmail.com"};
+    Spinner sp1, sp2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_asistencia);
+
+        btnEnviar =(Button)findViewById(R.id.btnEnviar);
+
+        ImageButton btnVolver = (ImageButton) findViewById(R.id.imageButton);
+
+        btnVolver.setImageResource(R.drawable.homeicon);
+
+        sp1 = (Spinner)findViewById(R.id.sp1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.asistereis_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp1.setAdapter(adapter);
+
+        sp2 = (Spinner)findViewById(R.id.sp2);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.asistentes_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp2.setAdapter(adapter2);
+    }
+
+    public void envioFormulario(View view){
 
         etNombres = (EditText)findViewById(R.id.etNombres);
         String nombre = etNombres.getText().toString();
@@ -27,18 +55,71 @@ public class MainActivityAsistencia extends AppCompatActivity {
         etComentarios = (EditText)findViewById(R.id.etComentarios);
         String comentarios = etComentarios.getText().toString();
 
-        tglBtn1 = (ToggleButton)findViewById(R.id.tglBtn1);
-        tglBtn2 = (ToggleButton)findViewById(R.id.tglBtn2);
-        tglBtn3 = (ToggleButton)findViewById(R.id.tglBtn3);
-        tglBtn4 = (ToggleButton)findViewById(R.id.tglBtn4);
-        tglBtnAceptar = (ToggleButton)findViewById(R.id.tglBtnAcpetar);
-        tglBtnRechazar = (ToggleButton)findViewById(R.id.tglBtnRechazar);
+        String boton1 = "";
+        String boton2 = "";
+        String boton3 = "";
+        String boton4 = "";
+        String boton5 = "";
 
-        btnEnviar =(Button)findViewById(R.id.btnEnviar);
+        String botonAceptar = "";
+        String botonRechazar = "";
 
-        ImageButton btnVolver = (ImageButton) findViewById(R.id.imageButton);
+        Intent itSend = new Intent(Intent.ACTION_SEND);
+        itSend.setType("text/html");
+        itSend.putExtra(Intent.EXTRA_EMAIL, TO);
+        itSend.putExtra(Intent.EXTRA_CC, email);
+        itSend.putExtra(Intent.EXTRA_SUBJECT, nombre);
 
-        btnVolver.setImageResource(R.drawable.homeicon);
+        String select1 = sp1.getSelectedItem().toString();
+        String select2 = sp2.getSelectedItem().toString();
+
+        if (select1 == "ACEPTAMOS ENCANTADOS"){
+            botonAceptar = "ACEPTAMOS ENCANTADOS";
+            Log.e("Leo", botonAceptar);
+            itSend.putExtra(Intent.EXTRA_TEXT, "Respuesta: "+botonAceptar);
+        }else if (select1 == "RECHAZAMOS CON PESAR"){
+            botonAceptar = "RECHAZAMOS CON PESAR";
+            Log.e("Leo", botonRechazar);
+            itSend.putExtra(Intent.EXTRA_TEXT, "Respuesta: "+botonAceptar);
+        }
+
+
+        if (select2 == "1"){
+            boton1 = "1";
+            Log.e("Leo", boton1);
+            itSend.putExtra(Intent.EXTRA_TEXT, "Asistentes: "+boton1);
+        }else if (select2 == "2"){
+            boton1 = "2";
+            Log.e("Leo", boton2);
+            itSend.putExtra(Intent.EXTRA_TEXT, "Asistentes: "+boton1);
+        }else if (select2 == "3"){
+            boton1 = "3";
+            Log.e("Leo", boton3);
+            itSend.putExtra(Intent.EXTRA_TEXT, "Asistentes: "+boton1);
+        }else if (select2 == "4"){
+            boton1 = "4";
+            Log.e("Leo", boton4);
+            itSend.putExtra(Intent.EXTRA_TEXT, "Asistentes: "+boton1);
+        }else if (select2 == "5"){
+            boton1 = "5";
+            Log.e("Leo", boton2);
+            itSend.putExtra(Intent.EXTRA_TEXT, "Asistentes: "+boton1);
+        }
+
+        String textoEmail = "Respuesta: "+botonAceptar +"\n Asistentes: "+boton1+"\n Comentario: "+comentarios ;
+
+        itSend.putExtra(Intent.EXTRA_TEXT, textoEmail);
+        try {
+            startActivity(itSend);
+            finish();
+            Log.e("Finished" , "seing email...");
+            Toast.makeText(MainActivityAsistencia.this,
+                    "Confirmacion enviada! Nos vemos en la boda!!", Toast.LENGTH_SHORT).show();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivityAsistencia.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+        //startActivity(itSend);
     }
 
     public void lanzaInicio(View view){
